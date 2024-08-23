c94a0ef99bcc990dc58aa8b913f35945525d2eb5
Fred Pitol <freirefrederico1@gmail.com>
2024-08-23 16:28:48 -0300

    Add project files.

---
+++ ServiceOrderManager/Data/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Diagnostics.Contracts;
+
+namespace ServiceOrderManager.Data
+{   // Herda de db context, using ctrl .
+    public class AppDbContext : DbContext
+    {
+        // 2 Construtor conexão
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) 
+        {
+
+        }
+
+    }
+}
