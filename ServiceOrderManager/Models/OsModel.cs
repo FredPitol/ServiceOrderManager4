8450cc0ca04582df330a67cb74f7b044fb7036ab
Fred Pitol <freirefrederico1@gmail.com>
2024-08-28 14:11:54 -0300

    15. Add theme

---
+++ ServiceOrderManager/Models/OsModel.cs
@@ -0,0 +1,16 @@
+﻿namespace ServiceOrderManager.Models
+{
+    public class OsModel
+    {
+        public int ID { get; set; }
+        public int ClientID { get; set; } 
+        public string Name { get; set; }
+        public ClientModel clientOS { get; set; } //Recebe informações do objeto cliente 
+        public double ServiceCost { get; set; }
+        public DateTime ExecutionDate  { get; set; } 
+        public string ServiceProviderCpf { get; set; }
+        public string ServiceProviderName { get; set; }
+        public string ServiceProviderRole { get; set; }
+
+    }
+}
--- ServiceOrderManager/Services/Client/IClientInterface.cs
+++ ServiceOrderManager/Services/Client/IClientInterface.cs
@@ -5,8 +5,7 @@ namespace ServiceOrderManager.Services.Client
 {
     public interface IClientInterface
     {
-        Task<ClientModel> CreateClient(DtoClientCreator dtoClientCreator, IFormFile photo); // Criação de metodo 
-
+        Task<ClientModel> CreateClient(DtoClientCreator dtoClientCreator, IFormFile photo);
         Task<List<ClientModel>> GetClients(); // 10.1 lista Todos os clientesz
         Task<ClientModel> GetClientById(int id); // 10.1 Retorna cliente especifico via ID
         Task<ClientModel> EditClient(ClientModel client, IFormFile? photo);
